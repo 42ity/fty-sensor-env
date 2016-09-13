b9773d239911ab0060ce93279121bbf68903ce36
Karol Hrdina <karolhrdina@eaton.com>
2016-09-13 15:47:55 +0200

    PROBLEM: memory leak
    Signed-off-by: Karol Hrdina <karolhrdina@eaton.com>

--- src/agent-th.cc
+++ src/agent-th.cc
@@ -392,6 +392,7 @@ main (int argc, char *argv []) {
 
             bios_proto_destroy (&msg);
             what++;
+            zstr_free (&topic);
         }
         if (zsys_interrupted) {
             zsys_warning ("interrupted ...  ");
