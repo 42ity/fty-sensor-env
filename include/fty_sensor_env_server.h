511c9fb89ae07e7f18b746ca4f4bbc684c1528a7
Jim Klimov <jim@jimklimov.com>
2020-04-23 12:00:10 +0200

    Bump copyrights to 2020

--- include/fty_sensor_env_server.h
+++ include/fty_sensor_env_server.h
@@ -1,7 +1,7 @@
 /*  =========================================================================
     fty_sensor_env_server - Grab temperature and humidity data from sensors attached to the box
 
-    Copyright (C) 2014 - 2018 Eaton
+    Copyright (C) 2014 - 2020 Eaton
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
